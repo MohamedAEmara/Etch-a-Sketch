6a84a3906610486b0f7eb2c579c2b25edd89a376
MohamedAEmara <medoemara104@gmail.com>
2023-05-17 14:24:54 +0300

    Fix some minor bugs & Update README.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+
 # Etch-a-Sketch
+
+[Live Demo](https://mohamedaemara.github.io/Etch-a-Sketch/) 
+
+
+A 16x16 Grid with 5 different colors to draw, by hovering on the cells.  
+
+it's an application on flexbox and creating elements with javacript 
+
--- temp.js
+++ temp.js
@@ -23,21 +23,10 @@ function buildGrid() {
 
 
 
-// const element = document.getElementsByClassName("cell");
-
-// const element = document.getElementById('cell').classList.add = 'selected';
-
-
 function mouseHover() {
     const element = document.querySelector('div');
 
-    // selected.addEventListener('click', changeColor);
     element.addEventListener('mouseover', changeColor);
-    // element.addEventListener('click', function()  {
-    //     const target = document.getElementsByClassName('selected');
-    //     target.innerHTML = 'jj';
-    // });
-    
     
 }
 
@@ -48,12 +37,7 @@ function changeColor(event) {
 
 function getColor(event){
     currColor = event.target.className;
-    // console.log(event.target);
-    // console.log(event.target.className);
-    // if(event.target.className == 'red'){
-    //     console.log('red pressed');
-    //     currColor = 'blue';
-    // }
+
 }
 
 function changeCurrentColor()
@@ -64,32 +48,15 @@ function changeCurrentColor()
     const yellow = document.querySelector('.yellow');
     const white = document.querySelector('.white');
     const cyan = document.querySelector('.cyan');
-    // const color = document.querySelectorAll('button');
     red.addEventListener('click', getColor);
     yellow.addEventListener('click', getColor);
     blue.addEventListener('click', getColor);
     white.addEventListener('click', getColor);
     cyan.addEventListener('click', getColor);
-    // red.addEventListener('click', getColor);
-    // const color = document.addEventListener('click', change)
+
 }
 
 
-
-function buildButtons() {
-    // var buttons = document.querySelector('.buttons');
-    // let green_btn = document.createElement('button');
-    // let red_btn = document.createElement('button');
-    // let black_btn = document.createElement('button');
-    // let cyan_btn = document.createElement('button');
-    // let yellow_btn = document.createElement('button');
-   
-    // buttons.appendChild(green_btn);
-    // buttons.appendChild(red_btn);
-    // buttons.appendChild(black_btn);
-    // buttons.appendChild(cyan_btn);
-    // buttons.appendChild(yellow_btn);
-}
 
 function changeButtonBackgrouns(event) {
     const btns = document.querySelectorAll('button');
@@ -101,6 +68,5 @@ function changeButtonBackgrouns(event) {
 
 buildGrid();
 changeButtonBackgrouns();
-buildButtons();
 mouseHover();
 changeCurrentColor();
